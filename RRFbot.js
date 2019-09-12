15c318b6c66c45e8974f11094b4c4bd21bcf6860
plscks <plscks@icloud.com>
2019-09-12 16:59:59 -0500

    Adds some RRFbot.js functionality

--- RRFbot.js
+++ RRFbot.js
@@ -6,8 +6,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var myArgs = process.argv.slice(2);
+var flag = myArgs[0];
+if (flag == '-t') {
+	var token = myArgs[1];
+}
+else {
+	console.log('Usage: node RRFbot.js [FLAG]');
+	console.log('FLAGS:');
+	console.log('        --help    This message');
+	console.log('        -t        Use auth token "-t [TOKEN-NUMBER]"');
+	process.exit();
+}
+
 client.once('ready', () => {
-	console.log('Ready!');
+console.log('Ready!');
 });
 
-client.login('your-token-goes-here');
+client.login(token);
+
+client.on('message', message => {
+	if (message.content.toLowerCase() === '!ping') {
+		message.channel.send('Pong.');
+	}
+});
