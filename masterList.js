64c7550bf35051c6792045b8456b1233b801bddc
plscks <plscks@icloud.com>
2019-09-29 12:16:21 -0500

    Testing

--- masterList.js
+++ masterList.js
@@ -1,10 +1,1 @@
-let masterListData = require('./masterList.json');
-console.log(masterListData['batch of leather']);
-var masterListArray = [];
-for (i in masterListData) {
-  masterListArray.push([i, masterListData [i]]);
-}
-console.log('masterListArray:\n' + masterListArray);
-for (var i = 0; i < masterListArray.length; ++i) {
-  console.log('Item: ' + masterListArray[i][2]);
-}
+
