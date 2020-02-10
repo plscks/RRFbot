3df7120e6bf1d3051945874f5d2ad91a6bba635c
plsck <plscks@icloud.com>
2020-02-09 18:22:08 -0600

    Database testing

--- databaseTest.js
+++ databaseTest.js
@@ -7,17 +7,35 @@
 //////////////
 // REQUIRES //
 //////////////
+const fs = require('fs');
 const shell = require('shelljs');
 const sqlite3 = require('sqlite3').verbose();
 ///////////////////
 // DATABASE INIT //
 ///////////////////
-let db = new sqlite3.Database('./Testbot', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the Testbot database.');
-});
+var dbFile = './Testbot';
+var dbExists = fs.existsSync(dbFile);
+
+if (!dbExists) {
+  fs.openSync(dbFile, 'w');
+}
+
+var db = new sqlite3.Database(dbFile);
+
+if (!dbExists) {
+  db.run('CREATE TABLE `your_table` (' +
+    '`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,' +
+    '`name` TEXT,' +
+    '`email` TEXT,');
+}
+
+// You can insert some data here in order to test
+var statement = db.prepare('INSERT INTO `your_table` (`name`, `email`) ' + 'VALUES (?, ?)');
+
+statement.run('Your name', 'some_random@email.com');
+statement.finalize();
+
+db.close();
 ///////////////////////////////////
 // COMMAND LINE ARGUMENT PARSING //
 ///////////////////////////////////
