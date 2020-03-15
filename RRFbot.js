8570c0b41fecae6ecc2e4f4a249c48bd815c9d32
plsck <plscks@icloud.com>
2020-03-15 09:47:53 -0500

    Still not working as intended

--- RRFbot.js
+++ RRFbot.js
@@ -561,8 +561,6 @@ async function covid19Args(myArgs, message) {
     if (listFlag === 'country') {
       let countryList = []
       let countryListDB = await covid19List('country', null);
-      console.log(`First: ${countryListDB.country}`);
-      console.log(`second: ${countryListDB.country[0]}`);
       console.log(`Country list: ${countryListDB}`);
     } else if (listFlag === 'province') {
       let countryArray = myArgs.slice(2, myArgs.length);
