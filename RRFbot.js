a442d0d5888666fb45253b340a0c599689cb04b8
plsck <plscks@icloud.com>
2020-03-22 10:40:28 -0500

    Trying for length again

--- RRFbot.js
+++ RRFbot.js
@@ -692,7 +692,7 @@ async function covid19Args(myArgs, message) {
         }});
         return
       }
-      message.channel.send(`localData[0].province: ${localData[0].province}`);
+      message.channel.send(`localData.length: ${localData.length}`);
       if (provinceLower.includes(query.toLowerCase())) {
         if (query.toLowerCase() === 'us') {
           message.channel.send({embed: {
