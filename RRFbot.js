dbd4ed62df12d0b8f337ec438e0f2c3a8752435f
plsck <plscks@icloud.com>
2020-04-20 06:23:02 -0500

    Fix for new member join

--- RRFbot.js
+++ RRFbot.js
@@ -167,20 +167,22 @@ job.start();
 // BOT FUNCTIONS //
 /////////////////////////////
 // NEW MEMBER JOIN MESSAGE //
-/////////////////////////////
+/////////////////////////////'
 client.on('guildMemberAdd', member => {
   var guildId = member.guild.id;
   var guildName = member.guild.name;
   if (guildId === '481612600149540875') {
-    console.log(`New member ${member} has joined ${guildName}!`);
-    var leader = message.guild.roles.find(role => role.name === "Leader");
-    member.guild.channels.get('481613088794083357').send('Welcome to the RRF! <@&' + leader + '> will be with you shortly to get you access.');
+    var guild = client.guilds.cache.get('481612600149540875');
+    console.log(`New member ${member.displayName} has joined ${guildName}!`);
+    var leader = guild.roles.cache.find(role => role.name === "Leader");
+    member.guild.channels.get('481613088794083357').send(`Welcome to the RRF! A ${leader} will be with you shortly to get you access.`);
   } else if (guildId === '564993020919808000') {
-    console.log(`New member ${member} has joined ${guildName}!`);
-    var srScientist = message.guild.roles.find(role => role.name === "Sr Scientist");
-    member.guild.channels.get('565313168482631680').send('Welcome to USF! A <@&' + srScientist + '> will be with you shortly to get you access.');
+    var guild = client.guilds.cache.get('564993020919808000');
+    console.log(`New member ${member.displayName} has joined ${guildName}!`);
+    var srScientist = guild.roles.cache.find(role => role.name === "Sr Scientist");
+    member.guild.channels.get('565313168482631680').send(`Welcome to USF! A ${srScientist} will be with you shortly to get you access.`);
   } else {
-    console.log(`New member ${member} has joined ${guildName}!`);
+    console.log(`New member ${member.displayName} has joined ${guildName}!`);
   }
 });
 
