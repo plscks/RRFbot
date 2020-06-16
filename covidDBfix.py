5dbff7734448c80ff2d24d09388b1d1bf584eaa7
plsck <plscks@icloud.com>
2020-06-16 07:18:55 -0500

    Updates DB to latest

--- covidDBfix.py
+++ covidDBfix.py
@@ -17,8 +17,8 @@ import sqlite3
 #from PIL import Image, ImageDraw, ImageFont
 #import adafruit_ssd1306
 
-date = '2020-06-13'
-urlDate = '06-12-2020'
+date = '2020-06-16'
+urlDate = '06-15-2020'
 
 def dataGrab():
     ## need to include an if that will roll the month back and set to the last day of previous if it is the first.
